e2614b4bd040edb99ebef46d33b3ba5ae1e44176
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-09 20:25:52 -0400

    message

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String DOB;
+    private String SSN;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+}
